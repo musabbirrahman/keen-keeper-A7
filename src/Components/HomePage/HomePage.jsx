2b813621d27bc355426c3e8a688cc5975bb9da0e
musabbirrahman <musabbirrahman490@gmail.com>
2026-04-17 12:45:19 +0600

    adjusted loading spinner

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -5,10 +5,10 @@ import AllFriends from './AllFriends/AllFriends';
 
 const HomePage = () => {
     return (
-        <div>
+        <div className="relative min-h-screen">
             <Banner></Banner>
             <Suspense fallback={
-                <div className='flex justify-center items-center w-full min-h-[50vh]'>
+                <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 p-4 bg-white/80 rounded-full shadow-md backdrop-blur-sm border border-gray-100'>
                     <span className="loading loading-spinner loading-lg text-[#244D3F]"></span>
                 </div>
             }>
